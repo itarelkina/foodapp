4ee3ac8f1531e1221bb9f943714268000ef160ce
Irina Tarelkina <itarelkina@yahoo.ca>
2020-12-05 17:32:58 -0500

    created models views forms and urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""mysite_2 URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from users import views as user_views
+from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/',include('food.urls')),
+    #path('register/',user_views.register,name='register'),
+    #path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    #path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    #path('profile/',user_views.profilepage,name='profile'),
 ]
