ac0e2ae25f387d5f0b91c3dcf4ce1b61b63239ea
Irina Tarelkina <itarelkina@yahoo.ca>
2020-12-06 09:58:03 -0500

    created models views forms and urls

--- mysite_2/wsgi.py
+++ mysite_2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for mysite_2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault ( 'DJANGO_SETTINGS_MODULE', 'mysite.settings' )
+os.environ.setdefault ( 'DJANGO_SETTINGS_MODULE', 'mysite_2.settings' )
 
 application = get_wsgi_application ()
