9d525f2728a6d45b77869ca8fe38159d9de7fb2f
Irina Tarelkina <itarelkina@yahoo.ca>
2020-12-08 16:44:56 -0500

    forms and User registration

--- mysite_2/settings.py
+++ mysite_2/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'food.apps.FoodConfig',
-    # 'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
